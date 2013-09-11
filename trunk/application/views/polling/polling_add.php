818161eac08519e3a7e56f50245871f487c3c44b
ujshandi@gmail.com <ujshandi@gmail.com@d32a530c-92e3-14c6-e55f-3b54ab3c0716>
2013-09-11 14:00:31 +0000

    backup

--- trunk/application/views/polling/polling_add.php
+++ trunk/application/views/polling/polling_add.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>Admin Page</title>
-	<meta name="description" content="Website Description" />
-	<meta name="keywords" content="Website Kwywords" />
-	<link rel="stylesheet" type="text/css" href="<?=base_url()?>public/css/backend.css" />
-	<script src="<?=base_url()?>public/js/jquery.js" type="text/javascript"></script>
-	<script src="<?=base_url()?>public/js/ui_core.js" type="text/javascript"></script>
-	<script src="<?=base_url()?>public/js/ui_tabs.js" type="text/javascript"></script>
-	<script src="<?=base_url()?>public/js/lightbox.js" type="text/javascript"></script>
+
 	<script type="text/javascript" src="<?=base_url()?>script/nicEdit.js"></script>
 	<script type="text/javascript">
 	bkLib.onDomLoaded(function() {
@@ -51,14 +40,7 @@
 		}
 	}
 	</script>
-	
-</head>
-<body>
-<div id="container">
-	<?=$headmenu?>
-		<div id="content">
-			<div id="maincontent">
-				<div class="boxbig">
+
 				<h1 class="titlebig">Tambah Polling</h1>
 					<div class="boxbigcontent">
 					<form method="post" action="<?=base_url()?>polling/simpan"  id="frmcontact">
@@ -83,22 +65,4 @@
 					</form>
 					</div>
 					<div class="boxbigcontentbottom"></div>
-				</div>
-				
-			</div>
 			
-			<?=$mainmenu?>
-			
-			<div class="clear"></div>
-		</div>
-		<div id="footer">
-			<p id="texttwitter"></p>
-			<ul id="menufooterright">
-				<li><?=COPYRIGHT?></li>
-				<li class="last"><a href="#">Back to top</a></li>
-			</ul>
-		</div>
-	</div>
-</div>
-</body>
-</html>
--- trunk/application/views/polling/polling_data.php
+++ trunk/application/views/polling/polling_data.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>Admin Page</title>
-	<meta name="description" content="Website Description" />
-	<meta name="keywords" content="Website Kwywords" />
-	<link rel="stylesheet" type="text/css" href="<?=base_url()?>public/css/backend.css" />
-	<script src="<?=base_url()?>public/js/jquery.js" type="text/javascript"></script>
-	<script src="<?=base_url()?>public/js/ui_core.js" type="text/javascript"></script>
-	<script src="<?=base_url()?>public/js/ui_tabs.js" type="text/javascript"></script>
-	<script src="<?=base_url()?>public/js/lightbox.js" type="text/javascript"></script>
+
 	<script type="text/javascript" src="<?=base_url()?>public/js/nicEdit.js"></script>
 	<script type="text/javascript">
 	bkLib.onDomLoaded(function() {
@@ -44,13 +33,6 @@
 	}
 	</script>
 	
-</head>
-<body>
-<div id="container">
-	<?=$headmenu?>
-		<div id="content">
-			<div id="maincontent">
-				<div class="boxbig">
 				<h1 class="titlebig">Polling</h1>
 					<div class="boxbigcontent">
 					<a href="<?=base_url()?>polling/add" class="button" >Tambah</a>
@@ -86,30 +68,4 @@
 					<?=$pagination?>
 					</div>
 					<div class="boxbigcontentbottom"></div>
-				</div>
 				
-			</div>
-			
-			<?=$mainmenu?>
-			
-			<div class="clear"></div>
-		</div>
-		<div id="footer">
-			<p id="texttwitter"></p>
-			<!--<ul id="menufooter">
-				<li><a href="#">Home</a></li>
-				<li><a href="#">News</a></li>
-				<li><a href="#">About Us</a></li>
-				<li><a href="#">Services</a></li>
-				<li><a href="#">Contact Us</a></li>
-				<li class="last"><a href="#">Follow us on Twitter</a></li>
-			</ul>-->
-			<ul id="menufooterright">
-				<li><?=COPYRIGHT?></li>
-				<li class="last"><a href="#">Back to top</a></li>
-			</ul>
-		</div>
-	</div>
-</div>
-</body>
-</html>
