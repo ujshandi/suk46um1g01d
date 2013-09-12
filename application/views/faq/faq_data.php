5deb36a492bd4d82fcc19fdf76f05e1d0400f492
ujshandi@gmail.com <ujshandi@gmail.com@d32a530c-92e3-14c6-e55f-3b54ab3c0716>
2013-09-12 03:40:28 +0000

    backup

--- application/views/faq/faq_data.php
+++ application/views/faq/faq_data.php
@@ -1,30 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
-
-<head>
-
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
-	<title>Admin Page</title>
-
-	<meta name="description" content="Website Description" />
-
-	<meta name="keywords" content="Website Kwywords" />
-
-	<link rel="stylesheet" type="text/css" href="<?=base_url()?>public/css/backend.css" />
-
-	<link rel="stylesheet" type="text/css" href="<?=base_url()?>public/media/jquery/ui.css" />
-
-	<script src="<?=base_url()?>public/js/jquery.js" type="text/javascript"></script>
-
-	<script src="<?=base_url()?>public/js/ui_core.js" type="text/javascript"></script>
-
-	<script src="<?=base_url()?>public/js/ui_tabs.js" type="text/javascript"></script>
-
-	<script src="<?=base_url()?>public/js/ui.dialog.js" type="text/javascript"></script>
-
-	<script src="<?=base_url()?>public/js/lightbox.js" type="text/javascript"></script>
 
 	<script type="text/javascript">
 
@@ -114,20 +87,6 @@
 
 	
 
-</head>
-
-<body>
-
-<div id="container">
-
-	<?=$headmenu?>
-
-		<div id="content">
-
-			<div id="maincontent">
-
-				<div class="boxbig">
-
 				<h1 class="titlebig">Daftar FAQ</h1>
 
 					<div class="boxbigcontent">
@@ -178,62 +137,3 @@
 
 					<div class="boxbigcontentbottom"></div>
 
-				</div>
-
-				
-
-			</div>
-
-			
-
-			<?=$mainmenu?>
-
-			
-
-			<div class="clear"></div>
-
-		</div>
-
-		<div id="footer">
-
-			<p id="texttwitter"></p>
-
-			<!--<ul id="menufooter">
-
-				<li><a href="#">Home</a></li>
-
-				<li><a href="#">News</a></li>
-
-				<li><a href="#">About Us</a></li>
-
-				<li><a href="#">Services</a></li>
-
-				<li><a href="#">Contact Us</a></li>
-
-				<li class="last"><a href="#">Follow us on Twitter</a></li>
-
-			</ul>-->
-
-			<ul id="menufooterright">
-
-				<li><?=COPYRIGHT?></li>
-
-				<li class="last"><a href="#">Back to top</a></li>
-
-			</ul>
-
-		</div>
-
-	</div>
-
-</div>
-
-<div id="dialog<?=$i?>" title="Detail Pesan Pengunjung" style="display:none"> 
-
-		wew
-
-</div>
-
-</body>
-
-</html>
